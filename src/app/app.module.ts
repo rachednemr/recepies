9b9ce226a76f03905e6bcd11f97afde3b2f286d1
Rached Nemr <rached.nemr@avonis.com>
2022-02-14 12:02:11 +0100

    49 - create app structure & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepiesListComponent } from './recepies-list/recepies-list.component';
+import { RecepiesDetailComponent } from './recepies/recepies-detail/recepies-detail.component';
+import { RecepiesItemComponent } from './recepies/recpies-list/recepies-item/recepies-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecepiesListComponent,
+    RecepiesDetailComponent,
+    RecepiesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
